5ccc993d56fff0f6413653c155195a5f556b6aad
Cosmin Adrian <cosminadrian218@gmail.com>
2022-03-26 16:28:47 +0200

    Revamp whole code, final version

--- src/index.js
+++ src/index.js
@@ -1,47 +1,20 @@
 import './style.css';
-import taskStatus from './status.js';
-import domObjects from './domObjects.js';
+import List from './list.js';
 
-let tasks = [];
-const clear = document.getElementById('clear');
-const inputField = document.getElementById('addTask');
-const displayOnLoad = () => {
-  if (localStorage.getItem('tasks') != null) {
-    domObjects();
-  }
-};
+const tasks = new List();
 
-displayOnLoad();
-
-const addToList = (newTask) => {
-  if (localStorage.getItem('tasks') === null) {
-    tasks = [];
-  } else {
-    tasks = JSON.parse(localStorage.getItem('tasks'));
-  }
-  tasks.push(newTask);
-  localStorage.setItem('tasks', JSON.stringify(tasks));
-};
-
-const tasksArr = () => {
-  const inputFieldValue = inputField.value;
-  const newTask = {
-    description: inputFieldValue,
-    completed: false,
-  };
-  addToList(newTask);
-};
-
-inputField.addEventListener('keypress', (e) => {
-  if (e.key === 'Enter') {
-    tasksArr();
-    domObjects();
-    taskStatus();
-  }
+document.querySelector('#add-task').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const activity = e.target.elements.activity.value;
+  tasks.addActivity(activity);
+  e.target.reset();
 });
 
-clear.addEventListener('click', () => {
-  localStorage.clear();
-  domObjects();
+document.querySelector('#delete-all').addEventListener('click', () => {
+  tasks.deleteAll();
 });
-taskStatus(0);
+
+// clear completed
+document.querySelector('.clear-completed').addEventListener('click', () => {
+  tasks.clearCompleted();
+});
